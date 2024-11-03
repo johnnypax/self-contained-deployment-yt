5dd6bcaeafa4361efa3dae3b1c6b1400ee9f594b
Giovanni Pace <g.pace@braintechinnovations.it>
2024-11-03 13:13:33 +0100

    Aggiunto comando di SCD

---
+++ README.md
@@ -0,0 +1,17 @@
+﻿# Pubblicare l'Applicazione come Self-Contained
+
+Esegui il comando di pubblicazione:
+
+```bash
+dotnet publish -c Release -r win-x64 --self-contained true
+```
+
+Sostituisci win-x64 con linux-x64 o osx-x64 se vuoi pubblicare per altri sistemi operativi.
+
+Questo comando creerà una cartella di output con tutti i file necessari per eseguire l’applicazione, incluso il runtime .NET. 
+
+La cartella sarà situata in:
+
+```bash
+bin/Release/net7.0/win-x64/publish
+```
---
+++ SCD_01_Introduzione_REST/SCD_01_Introduzione_REST/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SCD_01_Introduzione_REST.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Saluta() {
+            return Ok("Hello World");
+        }
+
+        [HttpGet("{varNominativo}")]
+        public IActionResult SalutaConNome(string varNominativo)
+        {  
+            return Ok($"Ciao {varNominativo}");
+        }
+    }
+}
